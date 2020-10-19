48b6f83792a8cfebaf6c904dc2511045803f1282
zempo <zelenkosolomon@gmail.com>
2020-10-19 11:29:59 -0700

    add register validation schema

---
+++ src/components/validation/registerSchema.js
@@ -0,0 +1,23 @@
+import * as yup from 'yup';
+
+export default yup.object().shape({
+    username: yup
+      .string()
+      .required("Please add a username.")
+      .min(2, "Username should be at least 2 chars."),
+    email: yup
+      .email("Please include a valid email.")
+      .required("Please include a valid email."),
+    password: yup
+      .string()
+      .required("Please include a password.")
+      .min(8, "Password should be at least 8 chars")
+      .max(92, "Password can be no more than 92 chars")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/,
+        "Please include at least 1 uppercase char, 1 number, and 1 special character."
+      ),
+    passwordMatch: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Make sure passwords are matching."),
+  });
