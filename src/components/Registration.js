fd163b0666149aec14416ee830e5b1872d4e48b6
zempo <zelenkosolomon@gmail.com>
2020-10-19 10:34:13 -0700

    first commit, new branch

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,4 +1,30 @@
+import React, { useState } from 'react'
 //user registration form
 //submit posts new user to back end
 //success returns login token
 //routes user to dashboard
+
+const initForm = {
+    username: '',
+    email: '',
+    password: ''
+}
+
+const initErr = {
+    username: '',
+    email: '',
+    password: ''
+}
+
+const Registration = () => {
+    const [register, setRegister] = useState(initForm)
+    const [errState, setErrState] = useState(initErr)
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Registration
