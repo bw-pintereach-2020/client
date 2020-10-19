c989a4d8c97c28adb57085a9d4a4f4c9d781282e
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-19 18:31:16 -0400

    deleted console logs from addBoard action and reducer

--- src/reducers/addBoardReducer.js
+++ src/reducers/addBoardReducer.js
@@ -11,13 +11,10 @@ function addBoardReducer(state = initialState, action) {
     const { type, payload } = action
     switch(type) {
         case POST_BOARD_START:
-            console.log('reducer start', payload)
             return {...state, inProgress: true}
         case POST_BOARD_SUCCESS:
-            console.log('reducer success', payload)
             return {...state, inProgress: false, board: payload}
         case POST_BOARD_FAILURE:
-            console.log('reducer failure', payload)
             return {...state, inProgress: false, error: payload}
         default:
             return state
