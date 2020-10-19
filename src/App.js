b2fcf8f1850401c50b805682964855d0925a1b08
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-18 20:36:26 -0400

    wiped style sheets, added component and styles folders, refactored App.test.js, wiped index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Pintereach</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
