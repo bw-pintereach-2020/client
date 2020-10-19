4e039145bdea5ec0c9a469a22c531cfc806ae262
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-10-19 13:22:06 -0700

    made input values reset when submitted

--- src/components/Login.js
+++ src/components/Login.js
@@ -38,11 +38,11 @@ export default function Login() {
         <form onSubmit={submit}>
             <label>
                 Username:
-                        <input type='text' name='username' onChange={inputChange} />
+                        <input type='text' name='username' onChange={inputChange} value={formValue.username}/>
             </label>
             <label>
                 Password:
-                        <input type='text' name='password' onChange={inputChange}/>
+                        <input type='text' name='password' onChange={inputChange} value={formValue.password}/>
             </label>
             <button>Login</button>
         </form>
