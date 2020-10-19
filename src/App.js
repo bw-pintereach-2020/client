e72fec2688268fcf3872fb2f78b92f310e5bf645
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-10-19 11:10:48 -0700

    addec home route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //react imports
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { Route, Link, Switch } from 'react-router-dom'
+import Navbar from './components/NavBar'
 import Home from './components/Home'
+
 
 //styles
 import './styles/App.css'
@@ -10,15 +12,14 @@ import './styles/App.css'
 function App() {
   return (
     <div className='App'>
-      {/* NavBar Component Here */}
-      <BrowserRouter>
+      {/* Navbar Component Here */}
+      <Navbar />
         <Switch>
           {/* <Route path='/login' component={Login} /> */}
           {/* <Route path='/login' component={Register} /> */}
           {/* <PrivateRoute path='/login' component={Dashboard} /> */}
-          <Route exact path='/' component={Home} />
+          <Route exact path='/' component={Home} ></Route> 
         </Switch>
-      </BrowserRouter>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,4 +2,15 @@
 //rendered by app outside router
 //logged out shows login and register
 //logged in shows logout and dashboard
+import React from 'react';
+import { Link } from 'react-router-dom';
 
+export default function NavBar(){
+    return (
+        <ul>
+            <Link to="/"><li>Home</li></Link>
+            <li>Login</li>
+            <li>Sign Up</li>
+        </ul>
+    );
+}
