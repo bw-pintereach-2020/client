81f7410dcf8de1255047a392f9d0239d21584dab
zempo <zelenkosolomon@gmail.com>
2020-10-20 23:35:12 -0700

    add helper functions

--- src/components/layout/NewsTile.js
+++ src/components/layout/NewsTile.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "../../styles/scss/Home.scss";
 
+const truncate = (str, max, suffix) => str.length < max ? str : `${str.substr(0, str.substr(0, max - suffix.length).lastIndexOf(' '))}${suffix}`;
+
+// Example
+truncate('This is a long message', 20, '...');  // 'This is a long...'
+
 const NewsTile = ({ article }) => {
   const { urlToImage, url, title } = article;
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
       <figure className='article-thumbnail'>
         <img src={urlToImage ? urlToImage : 'https://image.shutterstock.com/image-vector/business-analysis-icon-vector-illustrationmarketing-260nw-1685388379.jpg'} alt={title} />
-        <figcaption>{title}</figcaption>
+        <figcaption>{truncate(title, 40, '...')}</figcaption>
       </figure>
     </a>
   );
