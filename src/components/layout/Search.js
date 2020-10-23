98a425421bf3b7350f36ae96b21bd847289163bd
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2020-10-23 08:17:40 -0700

    fixed merge conflict

--- src/components/layout/Search.js
+++ src/components/layout/Search.js
@@ -8,14 +8,9 @@ export default function Search() {
 
     useEffect(() => {
         const getSearch = () => {
-<<<<<<< HEAD
             axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=news_channels&origin=*`)
                 .then(res => {
                     console.log(res.data.query.search, 'axios call')
-=======
-            axios.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=news_channel&origin=*`)
-                .then(res => {
->>>>>>> de6ce05a4bbcb674c4fdc11eedd1f92485fce6d5
                     setData(res.data.query.search);
                 })
                 .catch(err => {
@@ -46,23 +41,15 @@ export default function Search() {
         <section className="home-content">
             <h2>Explore Your Topic</h2>
             <form className='search-form' onSubmit={onSubmit}>
-<<<<<<< HEAD
                 <label>
                     Search:
-                    <input type='text' name="search" onChange={onChange} value={userSearch}/>
+                    <input type='text' name="search" onChange={onChange} value={userSearch} placeholder="News.."/>
                 </label>
-=======
-                <input type='text' name="search" placeholder='IRB' onChange={onChange}/>
->>>>>>> de6ce05a4bbcb674c4fdc11eedd1f92485fce6d5
                 <button>Search</button>
             </form>
             <div className="wiki-list">
                 {data.map((p, i) => (
-<<<<<<< HEAD
                     <a target="_blank"href={`https://en.wikipedia.org/wiki/${p.title}`}>
-=======
-                    <a href={`https://en.wikipedia.org/wiki/${p.title}`} target="_blank" rel="noopener noreferrer" key={i}>
->>>>>>> de6ce05a4bbcb674c4fdc11eedd1f92485fce6d5
                     <ul>
                         <img src="https://img.icons8.com/ios/452/wikipedia.png" alt="wikipedia icon"/>
                         <li>{p.title}</li>
